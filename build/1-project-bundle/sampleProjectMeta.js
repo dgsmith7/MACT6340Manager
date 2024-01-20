14af2e66c33f10613ad0326f14504b435c70259f
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2024-01-20 08:44:33 -0600

    added a few comments to sampleProjectMeta.js

--- build/1-project-bundle/sampleProjectMeta.js
+++ build/1-project-bundle/sampleProjectMeta.js
@@ -1,14 +1,15 @@
+// mySQL is sensitive - ensure you pay close attention to quotes and formats and datatypes
 export const projectInfo = {
   projectName: "Phi Geometry",
   artistsPayoutWallet: "0x9fcb72D67A4fE972FBEae178647f61A9EC3fF8f3",
   numberOfEditions: 2,
-  releaseDate: "2024-02-01 09:00:00",
+  releaseDate: "2024-02-01 09:00:00", // in UTC, keep exact format to maintain database
   websiteProjectDescription:
     "Three intersecting planes, all in the proportions of the Golden Ratio (Phi), rotate around a common axis.  The background color and color of each plane are determined by each token's unique hash.",
   price: 0.01,
   royaltiesPercent: 7.5,
   titleForViewport: "A Phi-based Shape in THREEjs",
-  projectWebUrl: "https://mact6340-app-nzv3s.ondigitalocean.app/projects",
+  projectWebUrl: "https://mact6340-app-nzv3s.ondigitalocean.app/projects", // full url as shown
   openSeaCollectionName: "Phi Geometry",
   openSeaCollectionDescription:
     "A Phi-based interactive shape rendered with THREEjs.",
